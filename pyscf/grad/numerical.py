010fa1be04ffdefc0d9629a301aef44ab2c7e415
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2022-07-17 17:51:34 -0500

    integrating to rhf_gradmixin base class but also needing the scanner object

--- pyscf/grad/numerical.py
+++ pyscf/grad/numerical.py
@@ -16,6 +16,19 @@
 # Author: Matthew Hennefarth <matthew.hennefarth@gmail.com>
 
 
+import numpy as np
+
+from pyscf.lib import logger, SinglePointScanner
+from pyscf.grad import rhf as rhf_grad
+
+def grad_elec(mc_grad, atmlst=None, verbose=None):
+    log = logger.new_logger(mc_grad, verbose)
+    mol = mc_grad.mol
+
+    # Generate a list of mols, and then call the energy function on each of them for parallelization
+    # Then, we store all of the data in the resulting shared_gradient object which has the same dimension as the mol.atms
+
+
 def as_scanner(grad):
     pass
 
@@ -23,9 +36,44 @@ def as_scanner(grad):
 class Gradients(rhf_grad.GradientsMixin):
 
     as_scanner = as_scanner
+    grad_elec = grad_elec
 
-    def kernel(self):
-        pass
+    def __init__(self, method, displacement=0.01):
+        self.displacement = displacement
 
-    
+        if isinstance(method, SinglePointScanner):
+            self.scanner = method
+            mc = method.base
+        
+        elif isinstance(method, rhf_grad.GradientsMixin):
+            self.scanner = method.base.as_scanner()
+            mc = method.base
+
+        elif getattr(method, "as_scanner", None):
+            self.scanner = method.as_scanner()
+            mc = method
+
+        else:
+            raise NotImplementedError('Electronic energy scanner of %s not available' %
+                                      method)
+
+        rhf_grad.GradientsMixin(mc)
+
+    def kernel(self, displacement=None, atmlst=None, verbose=None):
+        log = logger.new_logger(self, verbose)
+        if atmlst is None:
+            atmlst = self.atmlst
+        else:
+            self.atmlst = atmlst
+
+        if self.verbose >= logger.WARN:
+            self.check_sanity()
+        
+        if self.verbose >= logger.INFO:
+            self.dump_flags()
+
+        self.de = de = grad_elec(atmlst, log)
+
+        self._finalize()
+        return self.de
 
